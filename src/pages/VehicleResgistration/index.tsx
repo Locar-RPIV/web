0e78f57c7f86ed64e918461889c7e19cd906c284
jessica <jessicafavero.aluno@unipampa.edu.br>
2021-04-20 18:31:59 -0300

    feat: refactor

--- src/pages/VehicleResgistration/index.tsx
+++ src/pages/VehicleResgistration/index.tsx
@@ -37,23 +37,25 @@ const VehicleRegistration: React.FC = () => {
   const [cpfParceiro, setVehicleCpfParceiro] = useState("");
   const [filial, setVehicleFilial] = useState("");
   const [imageUrl, setVehicleImagemUrl] = useState("");
+  const [cilindradas, setVehicleCilindradas] = useState("");
+  const [portaMalas, setVehiclePortaMalas] = useState("");
 
   const history = useHistory();
 
   const optionsMarcaCarro = [
-    { value: "BMW", label: "BMW" },
-    { value: "Chevrolet", label: "Chevrolet" },
-    { value: "Fiat", label: "Fiat" },
-    { value: "Ford", label: "Ford" },
-    { value: "Honda", label: "Honda" },
-    { value: "Hyundai", label: "Hyundai" },
-    { value: "Kia", label: "Kia" },
-    { value: "Mercedes-Benz", label: "Mercedes-Benz" },
-    { value: "Mitsubishi", label: "Mitsubishi" },
-    { value: "Nissan", label: "Nissan" },
-    { value: "Toyota", label: "Toyota" },
-    { value: "Vokswagen", label: "Vokswagen" },
-    { value: "Jeep", label: "Jeep" },
+    { value: "BMW", label: "BMW" },
+    { value: "Chevrolet", label: "Chevrolet" },
+    { value: "Fiat", label: "Fiat" },
+    { value: "Ford", label: "Ford" },
+    { value: "Honda", label: "Honda" },
+    { value: "Hyundai", label: "Hyundai" },
+    { value: "Jeep", label: "Jeep" },
+    { value: "Kia", label: "Kia" },
+    { value: "Mercedes-Benz", label: "Mercedes-Benz" },
+    { value: "Mitsubishi", label: "Mitsubishi" },
+    { value: "Nissan", label: "Nissan" },
+    { value: "Toyota", label: "Toyota" },
+    { value: "Vokswagen", label: "Vokswagen" },
   ];
 
   const optionsNPortas = [
@@ -62,153 +64,135 @@ const VehicleRegistration: React.FC = () => {
   ];
 
   const optionsTipo = [
-    { value: "Carro", label: "Carro" },
-    { value: "Moto", label: "Moto" },
-    { value: "Bicicleta", label: "Bicicleta" },
-    { value: "Ônibus", label: "Ônibus" },
+    { value: 0, label: "Bicicleta" },
+    { value: 1, label: "Carro" },
+    { value: 2, label: "Moto" },
+    { value: 3, label: "Ônibus" },
   ];
 
   const optionsFilial = [
-    { value: "Unidade Alegrete", label: "Unidade Alegrete" },
-    { value: "Unidade Porto Alegre", label: "Unidade Porto Alegre" },
-    { value: "Unidade São Borja", label: "Unidade São Borja" },
-    { value: "Unidade Uruguaiana", label: "Unidade Uruguaiana" },
+    { value: "Unidade Alegrete", label: "Unidade Alegrete" },
+    { value: "Unidade Porto Alegre", label: "Unidade Porto Alegre" },
+    { value: "Unidade São Borja", label: "Unidade São Borja" },
+    { value: "Unidade Uruguaiana", label: "Unidade Uruguaiana" },
   ];
 
   const optionsModeloCarro = [
-    { value: "Onix", label: "Onix" }, //Chovrolet
-    { value: "HB20", label: "HB20" }, //Hyundai
-    { value: "Onix Plus", label: "Onix Plus" }, //Chovrolet
-    { value: "Strada", label: "Strada" }, //Fiat
-    { value: "Gol", label: "Gol" }, //Vokswagen
-    { value: "Ka", label: "Ka" }, //Ford
-    { value: "Argo", label: "Argo" }, //Fiat
-    { value: "T-Cross", label: "T-Cross" }, //Vokswagen
-    { value: "Renegade", label: "Renagade" }, //Jeep
-    { value: "Toro", label: "Toro" }, //Toro
-    { value: "GLA", label: "GLA" }, //Mercedes-Benz
-    { value: "X1", label: "X1" }, //BMW
-    { value: "SW4", label: "SW4" }, //Toyota
-    { value: "Classe C", label: "Classe C" }, //Mercedes-Benz
-    { value: "Série 3", label: "Série 3" }, //BMW
-    { value: "Sportage", label: "Sportage" }, //Kia
-    { value: "Cerato", label: "Cerato" }, //Kia
-    { value: "Outlander", label: "Outlander" }, //Mitsubishi
-    { value: "ASX", label: "ASX" }, //Mitsubishi
-    { value: "Pajero Sport", label: "Pajero Sport" }, //Mitsubishi
-    { value: "Leaf", label: "Leaf" }, //Nissan / Elétrico
-    { value: "March", label: "March" }, //Nissan
-    { value: "Versa", label: "Versa" }, //Nissan
-    { value: "Civic", label: "Civic" }, // Honda
-    { value: "Fit", label: "Fit" }, // Honda
-    { value: "City", label: "City" }, // Honda
+    { value: "Argo", label: "Argo" }, //Fiat
+    { value: "ASX", label: "ASX" }, //Mitsubishi
+    { value: "Cerato", label: "Cerato" }, //Kia
+    { value: "City", label: "City" }, // Honda
+    { value: "Civic", label: "Civic" }, // Honda
+    { value: "Classe C", label: "Classe C" }, //Mercedes-Benz
+    { value: "Fit", label: "Fit" }, // Honda
+    { value: "GLA", label: "GLA" }, //Mercedes-Benz
+    { value: "Gol", label: "Gol" }, //Vokswagen
+    { value: "HB20", label: "HB20" }, //Hyundai
+    { value: "Ka", label: "Ka" }, //Ford
+    { value: "Leaf", label: "Leaf" }, //Nissan / Elétrico
+    { value: "March", label: "March" }, //Nissan
+    { value: "Onix Plus", label: "Onix Plus" }, //Chovrolet
+    { value: "Onix", label: "Onix" }, //Chovrolet
+    { value: "Outlander", label: "Outlander" }, //Mitsubishi
+    { value: "Pajero Sport", label: "Pajero Sport" }, //Mitsubishi
+    { value: "Renegade", label: "Renegade" }, //Jeep
+    { value: "Série 3", label: "Série 3" }, //BMW
+    { value: "Sportage", label: "Sportage" }, //Kia
+    { value: "Strada", label: "Strada" }, //Fiat
+    { value: "SW4", label: "SW4" }, //Toyota
+    { value: "T-Cross", label: "T-Cross" }, //Vokswagen
+    { value: "Toro", label: "Toro" }, //Toro
+    { value: "Versa", label: "Versa" }, //Nissan
+    { value: "X1", label: "X1" }, //BMW
   ];
 
   const optionsPotencia = [
-    { value: "1.0", label: "1.0" },
-    { value: "1.2", label: "1.2" },
-    { value: "1.3", label: "1.3" },
-    { value: "1.4", label: "1.4" },
-    { value: "1.5", label: "1.5" },
-    { value: "1.6", label: "1.6" },
-    { value: "1.8", label: "1.8" },
-    { value: "2.0", label: "2.0" },
-    { value: "2.2", label: "2.2" },
-    { value: "2.3", label: "2.3" },
-    { value: "2.4", label: "2.4" },
-    { value: "2.5", label: "2.5" },
-    { value: "2.7", label: "2.7" },
-    { value: "2.8", label: "2.8" },
-    { value: "3.0", label: "3.0" },
-    { value: "3.2", label: "3.2" },
-    { value: "3.3", label: "3.3" },
-    { value: "3.5", label: "3.5" },
-    { value: "3.7", label: "3.7" },
-    { value: "3.8", label: "3.8" },
-    { value: "4.0", label: "4.0" },
+    // { value: "1.0", label: "1.0" },
+    // { value: "1.2", label: "1.2" },
+    // { value: "1.3", label: "1.3" },
+    // { value: "1.4", label: "1.4" },
+    // { value: "1.5", label: "1.5" },
+    // { value: "1.6", label: "1.6" },
+    // { value: "1.8", label: "1.8" },
+    // { value: "2.0", label: "2.0" },
+    // { value: "2.2", label: "2.2" },
+    // { value: "2.3", label: "2.3" },
+    // { value: "2.4", label: "2.4" },
+    // { value: "2.5", label: "2.5" },
+    // { value: "2.7", label: "2.7" },
+    // { value: "2.8", label: "2.8" },
+    // { value: "3.0", label: "3.0" },
+    // { value: "3.2", label: "3.2" },
+    // { value: "3.3", label: "3.3" },
+    // { value: "3.5", label: "3.5" },
+    // { value: "3.7", label: "3.7" },
+    // { value: "3.8", label: "3.8" },
+    // { value: "4.0", label: "4.0" },
+    { value: 10, label: 10 },
+    { value: 12, label: 12 },
+    { value: 13, label: 13 },
+    { value: 14, label: 14 },
+    { value: 15, label: 15 },
+    { value: 16, label: 16 },
+    { value: 18, label: 18 },
+    { value: 20, label: 20 },
+    { value: 22, label: 22 },
+    { value: 23, label: 23 },
+    { value: 24, label: 24 },
+    { value: 25, label: 25 },
+    { value: 27, label: 27 },
+    { value: 28, label: 28 },
+    { value: 30, label: 30 },
+    { value: 32, label: 32 },
+    { value: 33, label: 33 },
+    { value: 35, label: 35 },
+    { value: 37, label: 37 },
+    { value: 38, label: 38 },
+    { value: 40, label: 40 },
   ];
 
-  // // eslint-disable-next-line
-  // const optionsCilindradas = [ 
-  //   { value: "72 CV", label: "72 CV" },
-  //   { value: "75 CV", label: "75 CV" },
-  //   { value: "77 CV", label: "77 CV" },
-  //   { value: "78 CV", label: "78 CV" },
-  //   { value: "80 CV", label: "80 CV" },
-  //   { value: "82 CV", label: "82 CV" },
-  //   { value: "84 CV", label: "84 CV" },
-  //   { value: "85 CV", label: "85 CV" },
-  //   { value: "88 CV", label: "88 CV" },
-  //   { value: "101 CV", label: "101 CV" },
-  //   { value: "104 CV", label: "104 CV" },
-  //   { value: "109 CV", label: "109 CV" },
-  //   { value: "110 CV", label: "110 CV" },
-  //   { value: "111 CV", label: "111 CV" },
-  //   { value: "114 CV", label: "114 CV" },
-  //   { value: "115 CV", label: "115 CV" },
-  //   { value: "116 CV", label: "116 CV" },
-  //   { value: "120 CV", label: "120 CV" },
-  //   { value: "123 CV", label: "123 CV" },
-  //   { value: "128 CV", label: "128 CV" },
-  //   { value: "130 CV", label: "130 CV" },
-  //   { value: "135 CV", label: "135 CV" },
-  //   { value: "136 CV", label: "136 CV" },
-  //   { value: "139 CV", label: "139 CV" },
-  //   { value: "149 CV", label: "149 CV" },
-  //   { value: "150 CV", label: "150 CV" },
-  //   { value: "155 CV", label: "155 CV" },
-  //   { value: "156 CV", label: "156 CV" },
-  //   { value: "157 CV", label: "157 CV" },
-  //   { value: "160 CV", label: "160 CV" },
-  //   { value: "163 CV", label: "163 CV" },
-  //   { value: "165 CV", label: "165 CV" },
-  //   { value: "167 CV", label: "167 CV" },
-  //   { value: "170 CV", label: "170 CV" },
-  //   { value: "171 CV", label: "171 CV" },
-  //   { value: "184 CV", label: "184 CV" },
-  //   { value: "190 CV", label: "190 CV" },
-  //   { value: "192 CV", label: "192 CV" },
-  //   { value: "204 CV", label: "204 CV" },
-  //   { value: "211 CV", label: "211 CV" },
-  //   { value: "231 CV", label: "231 CV" },
-  //   { value: "240 CV", label: "240 CV" },
-  //   { value: "292 CV", label: "292 CV" },
-  //   { value: "387 CV", label: "387 CV" },
-  // ];
-
   const optionsAno = [
-    { value: "1990", label: "1990" },
-    { value: "1991", label: "1991" },
-    { value: "1992", label: "1992" },
-    { value: "1993", label: "1993" },
-    { value: "1994", label: "1994" },
-    { value: "1995", label: "1995" },
-    { value: "1996", label: "1996" },
-    { value: "1997", label: "1997" },
-    { value: "1998", label: "1998" },
-    { value: "1999", label: "1999" },
-    { value: "2000", label: "2000" },
-    { value: "2001", label: "2001" },
-    { value: "2002", label: "2002" },
-    { value: "2003", label: "2003" },
-    { value: "2004", label: "2004" },
-    { value: "2005", label: "2005" },
-    { value: "2006", label: "2006" },
-    { value: "2007", label: "2007" },
-    { value: "2008", label: "2008" },
-    { value: "2009", label: "2009" },
-    { value: "2010", label: "2010" },
-    { value: "2011", label: "2011" },
-    { value: "2012", label: "2012" },
-    { value: "2013", label: "2013" },
-    { value: "2014", label: "2014" },
-    { value: "2015", label: "2015" },
-    { value: "2016", label: "2016" },
-    { value: "2017", label: "2017" },
-    { value: "2018", label: "2018" },
-    { value: "2019", label: "2019" },
-    { value: "2020", label: "2020" },
-    { value: "2021", label: "2021" },
+    { value: 1990, label: 1990},
+    { value: 1991, label: 1991},
+    { value: 1992, label: 1992},
+    { value: 1993, label: 1993},
+    { value: 1994, label: 1994},
+    { value: 1995, label: 1995},
+    { value: 1996, label: 1996},
+    { value: 1997, label: 1997},
+    { value: 1998, label: 1998},
+    { value: 1999, label: 1999},
+    { value: 2000, label: 2000},
+    { value: 2001, label: 2001},
+    { value: 2002, label: 2002},
+    { value: 2003, label: 2003},
+    { value: 2004, label: 2004},
+    { value: 2005, label: 2005},
+    { value: 2006, label: 2006},
+    { value: 2007, label: 2007},
+    { value: 2008, label: 2008},
+    { value: 2009, label: 2009},
+    { value: 2010, label: 2010},
+    { value: 2011, label: 2011},
+    { value: 2012, label: 2012},
+    { value: 2013, label: 2013},
+    { value: 2014, label: 2014},
+    { value: 2015, label: 2015},
+    { value: 2016, label: 2016},
+    { value: 2017, label: 2017},
+    { value: 2018, label: 2018},
+    { value: 2019, label: 2019},
+    { value: 2020, label: 2020},
+    { value: 2021, label: 2021},
+  ];
+
+  const optionsCombustivel = [
+    { value: "Diesel", label: "Diesel" },
+    { value: "Etanol", label: "Etanol" },
+    { value: "Flex", label: "Flex" },
+    { value: "Gasolina", label: "Gasolina" },
+    { value: "GNV", label: "GNV" },
+    { value: "Híbrido/ Elétrico", label: "Híbrido/ Elétrico" },
   ];
 
   //   const optionsCor = [
@@ -226,13 +210,90 @@ const VehicleRegistration: React.FC = () => {
   //     { value: "Burro quando foge", label: "Burro quando foge"},
   // ];
 
-  const optionsCombustivel = [
-    { value: "Etanol", label: "Etanol" },
-    { value: "Gasolina", label: "Gasolina" },
-    { value: "GNV", label: "GNV" },
-    { value: "Diesel", label: "Diesel" },
-    { value: "Flex", label: "Flex" },
-    { value: "Híbrido/ Elétrico", label: "Híbrido/ Elétrico" },
+  const optionsPortaMalas = [
+    { value: "146 Litros", label: "146 Litros" },
+    { value: "180 Litros", label: "180 Litros" },
+    { value: "257 Litros", label: "257 Litros" },
+    { value: "265 Litros", label: "265 Litros" },
+    { value: "275 Litros", label: "275 Litros" },
+    { value: "276 Litros", label: "276 Litros" },
+    { value: "285 Litros", label: "285 Litros" },
+    { value: "289 Litros", label: "289 Litros" },
+    { value: "300 Litros", label: "300 Litros" },
+    { value: "313 Litros", label: "313 Litros" },
+    { value: "320 Litros", label: "320 Litros" },
+    { value: "334 Litros", label: "334 Litros" },
+    { value: "359 Litros", label: "359 Litros" },
+    { value: "363 Litros", label: "363 Litros" },
+    { value: "400 Litros", label: "400 Litros" },
+    { value: "415 Litros", label: "415 Litros" },
+    { value: "420 Litros", label: "420 Litros" },
+    { value: "433 Litros", label: "433 Litros" },
+    { value: "435 Litros", label: "435 Litros" },
+    { value: "445 Litros", label: "445 Litros" },
+    { value: "449 Litros", label: "449 Litros" },
+    { value: "450 Litros", label: "450 Litros" },
+    { value: "460 Litros", label: "460 Litros" },
+    { value: "480 Litros", label: "480 Litros" },
+    { value: "491 Litros", label: "491 Litros" },
+    { value: "505 Litros", label: "505 Litros" },
+    { value: "519 Litros", label: "519 Litros" },
+    { value: "520 Litros", label: "520 Litros" },
+    { value: "560 Litros", label: "560 Litros" },
+    { value: "605 Litros", label: "605 Litros" },
+    { value: "715 Litros", label: "715 Litros" },
+    { value: "820 Litros", label: "820 Litros" },
+    { value: "868 Litros", label: "868 Litros" },
+    { value: "971 Litros", label: "971 Litros" },
+    { value: "1100 Litros", label: "1100 Litros" },
+    { value: "1354 Litros", label: "1354 Litros" },    
+  ];
+
+  const optionsCilindradas = [
+    { value: "72 CV", label: "72 CV" },
+    { value: "75 CV", label: "75 CV" },
+    { value: "77 CV", label: "77 CV" },
+    { value: "78 CV", label: "78 CV" },
+    { value: "80 CV", label: "80 CV" },
+    { value: "82 CV", label: "82 CV" },
+    { value: "84 CV", label: "84 CV" },
+    { value: "85 CV", label: "85 CV" },
+    { value: "88 CV", label: "88 CV" },
+    { value: "101 CV", label: "101 CV" },
+    { value: "104 CV", label: "104 CV" },
+    { value: "109 CV", label: "109 CV" },
+    { value: "110 CV", label: "110 CV" },
+    { value: "111 CV", label: "111 CV" },
+    { value: "114 CV", label: "114 CV" },
+    { value: "115 CV", label: "115 CV" },
+    { value: "116 CV", label: "116 CV" },
+    { value: "120 CV", label: "120 CV" },
+    { value: "123 CV", label: "123 CV" },
+    { value: "128 CV", label: "128 CV" },
+    { value: "130 CV", label: "130 CV" },
+    { value: "135 CV", label: "135 CV" },
+    { value: "136 CV", label: "136 CV" },
+    { value: "139 CV", label: "139 CV" },
+    { value: "149 CV", label: "149 CV" },
+    { value: "150 CV", label: "150 CV" },
+    { value: "155 CV", label: "155 CV" },
+    { value: "156 CV", label: "156 CV" },
+    { value: "157 CV", label: "157 CV" },
+    { value: "160 CV", label: "160 CV" },
+    { value: "163 CV", label: "163 CV" },
+    { value: "165 CV", label: "165 CV" },
+    { value: "167 CV", label: "167 CV" },
+    { value: "170 CV", label: "170 CV" },
+    { value: "171 CV", label: "171 CV" },
+    { value: "184 CV", label: "184 CV" },
+    { value: "190 CV", label: "190 CV" },
+    { value: "192 CV", label: "192 CV" },
+    { value: "204 CV", label: "204 CV" },
+    { value: "211 CV", label: "211 CV" },
+    { value: "231 CV", label: "231 CV" },
+    { value: "240 CV", label: "240 CV" },
+    { value: "292 CV", label: "292 CV" },
+    { value: "387 CV", label: "387 CV" },
   ];
 
   const handleSubmitVehicle = async (e: { preventDefault: () => void }) => {
@@ -254,6 +315,8 @@ const VehicleRegistration: React.FC = () => {
     console.log(cpfParceiro);
     console.log(filial);
     console.log(imageUrl);
+    console.log(cilindradas);
+    console.log(portaMalas);
 
     try {
       if (
@@ -263,12 +326,14 @@ const VehicleRegistration: React.FC = () => {
         !cor ||
         !ano ||
         !tipoCombustivel ||
-        !numeroPortas ||
+        // !numeroPortas ||
         !quilometragem ||
         !renavan ||
         !chassi ||
         !valorLocacao ||
         !filial ||
+        // !cilindradas ||
+        // !portaMalas ||
         !imageUrl
       ) {
         alert("Preencha todos os campos");
@@ -282,7 +347,7 @@ const VehicleRegistration: React.FC = () => {
           cor,
           ano,
           tipoCombustivel,
-          numeroPortas,
+          // numeroPortas,
           quilometragem,
           renavan,
           chassi,
@@ -290,6 +355,8 @@ const VehicleRegistration: React.FC = () => {
           carroParceiro: true,
           cpfParceiro,
           filial,
+          // cilindradas,
+          // portaMalas,
           imageUrl,
         });
         history.push("/FeedbackVehicle");
@@ -304,7 +371,7 @@ const VehicleRegistration: React.FC = () => {
           cor,
           ano,
           tipoCombustivel,
-          numeroPortas,
+          // numeroPortas,
           quilometragem,
           renavan,
           chassi,
@@ -312,6 +379,8 @@ const VehicleRegistration: React.FC = () => {
           carroParceiro: false,
           cpfParceiro: 0,
           filial,
+          // cilindradas,
+          // portaMalas,
           imageUrl,
         });
         history.push("/FeedbackVehicle");
@@ -344,7 +413,7 @@ const VehicleRegistration: React.FC = () => {
                 options={optionsModeloCarro}
                 name="vehicle_modelo"
                 placeholder="Modelo"
-                onChange={(e) => setVehicleModelo(e.value)} 
+                onChange={(e) => setVehicleModelo(e.value)}
               />
               <Select
                 options={optionsCombustivel}
@@ -360,6 +429,12 @@ const VehicleRegistration: React.FC = () => {
               />
             </div>
             <div className="three-inputs">
+              <Select
+                options={optionsCilindradas}
+                name="vehicle_cilindradas"
+                placeholder="Cilindradas"
+                onChange={(e) => setVehicleCilindradas(e.value)}
+              />
               <Select
                 options={optionsPotencia}
                 name="vehicle_potencia"
@@ -377,6 +452,12 @@ const VehicleRegistration: React.FC = () => {
                 name="vehicle_nPortas"
                 placeholder="Nº de Portas"
                 onChange={(e) => setVehiclePortas(e.value)}
+              />
+              <Select
+                options={optionsPortaMalas}
+                name="vehicle_portaMalas"
+                placeholder="Capacidade Porta Malas"
+                onChange={(e) => setVehiclePortaMalas(e.value)}
               />
             </div>
             <div className="three-inputs">
