e86d6b02edc6e5a4e0f574d2c9b37807dcc0a195
Brandow Buenos <brandowbuenos@gmail.com>
2021-06-29 16:23:11 -0300

    refactor(Users): organize imports

--- src/pages/Dashboard/Users/index.tsx
+++ src/pages/Dashboard/Users/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { MdPerson } from "react-icons/md";
-import NavBar from "../../../components/NavBar";
 
+import NavBar from "../../../components/NavBar";
 import {
   Container,
   Card,
