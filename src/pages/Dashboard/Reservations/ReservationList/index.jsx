d9956a8e60e44e068ee089d58388e7ebaa8189e1
carolinemdss <Carolinesilveira.aluno@unipampa.edu.br>
2021-06-29 17:07:43 -0300

    refactor(ReservationList): organize imports, remove unnused code and console logs

--- src/pages/Dashboard/Reservations/ReservationList/index.jsx
+++ src/pages/Dashboard/Reservations/ReservationList/index.jsx
@@ -1,84 +1,81 @@
 import React, { useState, useEffect } from "react";
-import { ListUserCard, Title, EditButton, DeleteButton } from "./styles";
-
+import { IoTicket } from "react-icons/io5";
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
 
 import api from "../../../../services/api";
-import { IoTicket } from "react-icons/io5";
+import { ListUserCard, Title, EditButton, DeleteButton } from "./styles";
 
 const ReservationList = () => {
-    const [reservations, setReservation] = useState([]);
+  const [reservations, setReservation] = useState([]);
 
-    const fetchUsers = async () => {
-      const { data } = await api.get(
-        "https://apirestful-locar.herokuapp.com/api/reservation"
-      );
-      const reservations = data;
-      setReservation(reservations);
-      console.log(reservations);
-    };
-  
-    useEffect(() => {
-      fetchUsers();
-    }, []);
-  
+  const fetchUsers = async () => {
+    const { data } = await api.get(
+      "https://apirestful-locar.herokuapp.com/api/reservation"
+    );
+    setReservation(data);
+  };
 
-    function deleteReservation(reservation) {
-      try {
-        api
-          .delete(
-            `https://apirestful-locar.herokuapp.com/api/reservation/${reservation.id}`
-          )
-          .then((res) => {
-            alert("Reserva deletada com sucesso");
-            window.location.reload();
-          });
-      } catch (e) {
-        console.log(e);
-        alert("Ocorreu um erro ao tentar deletar a reserva");
-      }
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  function deleteReservation(reservation) {
+    try {
+      api
+        .delete(
+          `https://apirestful-locar.herokuapp.com/api/reservation/${reservation.id}`
+        )
+        .then((res) => {
+          alert("Reserva deletada com sucesso");
+          window.location.reload();
+        });
+    } catch (e) {
+      alert("Ocorreu um erro ao tentar deletar a reserva");
     }
+  }
 
   return (
     <ListUserCard>
-    <Title>
-      <IoTicket className="title-icon" size={"1.3em"} />
-      Reservas
-    </Title>
-    <div className="table-content">
-    <table>
-      <tr>
-        <th>Código</th>
-        <th>Nome</th>
-        <th>Véiculo</th>
-        <th>Data da retirada</th>
-        <th>Ações</th>
-      </tr>
+      <Title>
+        <IoTicket className="title-icon" size={"1.3em"} />
+        Reservas
+      </Title>
+      <div className="table-content">
+        <table>
+          <tr>
+            <th>Código</th>
+            <th>Nome</th>
+            <th>Véiculo</th>
+            <th>Data da retirada</th>
+            <th>Ações</th>
+          </tr>
 
-      {reservations.map((reservation) => {
-        return [
-          <>
-            <tr>
-              <td>{reservation.id}</td>
-              <td>{reservation.user.nome}</td>
-              <td>{reservation.veiculo.placa}</td>
-              <td>{reservation.dataRetirada}</td>
-              <td>
-                  <EditButton>
-                    <FaEdit size={"2em"}/>
-                  </EditButton>
+          {reservations.map((reservation) => {
+            return [
+              <>
+                <tr>
+                  <td>{reservation.id}</td>
+                  <td>{reservation.user.nome}</td>
+                  <td>{reservation.veiculo.placa}</td>
+                  <td>{reservation.dataRetirada}</td>
+                  <td>
+                    <EditButton>
+                      <FaEdit size={"2em"} />
+                    </EditButton>
                     &nbsp;&nbsp;&nbsp;&nbsp;
-                    <DeleteButton onClick={() => deleteReservation(reservation)}>
-                      <FaTrashAlt size={"2em"}/>
+                    <DeleteButton
+                      onClick={() => deleteReservation(reservation)}
+                    >
+                      <FaTrashAlt size={"2em"} />
                     </DeleteButton>
-              </td>
-            </tr>
-          </>,
-        ];
-      })}
-    </table>
-    </div>
-  </ListUserCard>
+                  </td>
+                </tr>
+              </>,
+            ];
+          })}
+        </table>
+      </div>
+    </ListUserCard>
   );
 };
 
